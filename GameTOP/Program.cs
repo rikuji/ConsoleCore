08d5b4adf03e4f319ed50c233990dfed39649d3e
lucasbezerra <lucasbezerra@serel.com.br>
2019-12-23 11:46:04 -0300

    First Commit

---
+++ GameTOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GameTOP.lib;
+
+namespace GameTOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jogo = new JogoFODA(
+                new Jogador3(),
+                new Jogador1("Lucas")
+
+            );
+            jogo.IniciarJogo();
+        }
+    }
+}
